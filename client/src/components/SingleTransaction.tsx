4d641ced04d35e85e4499a6a7fd0321c85c90fe7
Ikraam <ibndaanis@gmail.com>
2023-10-11 15:53:55 +0100

    updated styling

--- client/src/components/SingleTransaction.tsx
+++ client/src/components/SingleTransaction.tsx
@@ -41,35 +41,47 @@ const SingleTransaction: React.FC<SingleTransactionProps> = ({ id }) => {
   // Converting WEI to ETH so it's readable and rounding it to 2 decimal places (TASK #7)
   const ethValue = Number(ethers.formatEther(value || '0').toString());
 
+  console.log(data?.getTransaction);
+
   return (
     <div>
       <div className="flex flex-col mt-20">
-        <div className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+        <div className="max-w-[85rem] w-full mx-auto p-4 sm:flex sm:items-center sm:justify-between">
           <div>
             <button
               onClick={handleGoBack}
               type="button"
-              className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-full border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
+              className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
             >
               Go back
             </button>
           </div>
         </div>
       </div>
-      <div className="mt-20">
-        <div className="max-w-[85rem] w-full mx-auto px-4">
-          <h1 className="text-2xl mb-10">Transaction</h1>
-          <p>
-            <span className="font-bold">Transaction Hash:</span> {hash}
+      <div className="mt-10 p-4 max-w-[85rem] mx-auto">
+        <h1 className="text-2xl mb-4 font-semibold">Transaction Details</h1>
+        <div className="w-full mx-auto p-4 bg-white border-zinc-200 rounded-sm border flex flex-col gap-2">
+          <p className="break-words">
+            <span className="font-semibold inline-block w-full min-w-[10rem] sm:w-[25%]">
+              Transaction Hash:{' '}
+            </span>
+            <span className="break-words">{hash}</span>
           </p>
-          <p>
-            <span className="font-bold">Sender Address:</span> {from}
+          <p className="break-words">
+            <span className="font-semibold inline-block min-w-[10rem] sm:w-[25%]">
+              Sender Address:{' '}
+            </span>
+            <span className="text-blue-600">{from}</span>
           </p>
-          <p>
-            <span className="font-bold">Recipient Address:</span> {to}
+          <p className="break-words">
+            <span className="font-semibold inline-block min-w-[10rem] sm:w-[25%]">
+              Recipient Address:{' '}
+            </span>
+            <span className="text-blue-600">{to}</span>
           </p>
-          <p>
-            <span className="font-bold">Amount:</span> {ethValue} ETH
+          <p className="break-words">
+            <span className="font-semibold inline-block min-w-[10rem] sm:w-[25%]">Amount: </span>
+            <span>{ethValue} ETH</span>
           </p>
         </div>
       </div>
--- client/src/components/TransactionsList.tsx
+++ client/src/components/TransactionsList.tsx
@@ -21,7 +21,7 @@ const TransactionList: React.FC = () => {
   if (loading) {
     return (
       <div className="flex flex-col mt-20">
-        <div className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+        <div className="max-w-[85rem] w-full mx-auto p-4 sm:flex sm:items-center sm:justify-between">
           Loading...
         </div>
       </div>
@@ -31,7 +31,7 @@ const TransactionList: React.FC = () => {
   if (error) {
     return (
       <div className="flex flex-col mt-20">
-        <div className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between text-red-600 font-bold">
+        <div className="max-w-[85rem] w-full mx-auto p-4 sm:flex sm:items-center sm:justify-between text-red-600 font-bold">
           Error: {error.message}
         </div>
       </div>
@@ -40,8 +40,8 @@ const TransactionList: React.FC = () => {
 
   return (
     <div className="flex flex-col mt-20">
-      <div className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
-        <div className="p-1.5 min-w-full inline-block align-middle">
+      <div className="max-w-[85rem] w-full mx-auto p-4 sm:flex sm:items-center sm:justify-between">
+        <div className="p-1.5 w-full inline-block align-middle">
           {!!transactions.length ? (
             <>
               {transactions.map(({ hash, to, from, value }) => {
@@ -51,13 +51,15 @@ const TransactionList: React.FC = () => {
                 return (
                   <div
                     key={hash}
-                    className="bg-white shadow-sm p-4 md:p-5 border rounded border-gray-300 mt-3 hover:border-blue-500 cursor-pointer"
+                    className="bg-white shadow-sm p-4 md:p-5 border rounded border-gray-300 mt-3 hover:border-blue-500 cursor-pointer w-full break-words overflow-hidden text-ellipsis"
                     onClick={() => handleNavigate(hash)}
                     title={`${ethValue} ETH`}
                   >
-                    <span className="font-bold">{ethValue.toFixed(2)} ETH</span> sent from{' '}
-                    <span className="font-bold">{from}</span> to{' '}
-                    <span className="font-bold">{to}</span>
+                    <span className="font-bold break-words text-blue-600">
+                      {ethValue.toFixed(2)} ETH
+                    </span>{' '}
+                    sent from <span className="font-bold break-words">{from}</span> to{' '}
+                    <span className="font-bold break-words">{to}</span>
                   </div>
                 );
               })}
